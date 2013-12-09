7a2a026e73b5ae01364a2bc400c3c260d6f6e08f
Andrew Dryga <andrew@dryga.com>
2013-12-09 20:30:32 +0200

    mailchimp integration

--- www/form.php
+++ www/form.php
@@ -4,20 +4,7 @@ ini_set('display_errors', true);
 
 require_once(__DIR__.'/../vendor/autoload.php');
 
-$mc = new Mailchimp('2e371d9fe765a8c1c7f1a1586a281bdf-us7');
-
-$params = array(
-	"id" => '23207a4ce3',
-    "email" => array(
-        "email" => filter_var($_REQUEST['email'], FILTER_SANITIZE_EMAIL),
-    ),
-    'double_optin' => false
-);
-
-if(isset($_REQUEST) && is_array($_REQUEST) && array_key_exists('json', $_REQUEST)) {
-  echo json_encode($mc->call('lists/subscribe', $params));
-} else {
-  $mc->call('lists/subscribe', $params);
+function showPage($title, $data) {
   echo <<<HTML
   <!DOCTYPE HTML>
   <html lang="en-US">
@@ -29,12 +16,32 @@ if(isset($_REQUEST) && is_array($_REQUEST) && array_key_exists('json', $_REQUEST
               window.location.href = "/";
             }, 5000);
           </script>
-          <title>Перенаправление</title>
+          <title>{$title}</title>
       </head>
       <body>
-        Ваши данные отправленны, если в течении 5 секунд вы не будете перенаправлены обратно на сайт,
-        то нажмите на <a href="/">ссылку</a>.
+        {$data}<br />
       </body>
   </html>
 HTML;
+}
+
+if(!isset($_REQUEST)) {
+  exit('No request data');
+}
+
+if(!array_key_exists('email', $_REQUEST)) {
+  exit('Email is not set');
+}
+
+$email = filter_var($_REQUEST['email'], FILTER_SANITIZE_EMAIL);
+
+$merge_vars = [];
+$merge_vars['EMAIL'] = $email ?: 'noinfo'.microtime(true).'@dontmail.me';
+$mc = new Mailchimp('2e371d9fe765a8c1c7f1a1586a281bdf-us7');
+$result = $mc->lists->subscribe('3dd249cb0d', ['email' => $email], $merge_vars, 'html', false, true);
+
+if(array_key_exists('type', $_REQUEST) && $_REQUEST['type'] == 'json') {
+  echo json_encode($result);
+} else {
+  showPage('Перенаправление', 'Ваши данные отправленны, если в течении 5 секунд вы не будете перенаправлены обратно на сайт, то нажмите на <a href="/">ссылку</a>.');
 }
