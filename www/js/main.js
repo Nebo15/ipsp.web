0ccc0404652812c6447dde036d7464ed55cfc429
Andrew Dryga <andrew@dryga.com>
2013-12-13 18:38:26 +0200

    New design

--- www/js/main.js
+++ www/js/main.js
@@ -1,55 +1,116 @@
-var launchPage = {
-	url: 'submit.html',
-	sent: false,
-	formBgWidth: 120
-}
-$(document).ready( function() {
-	$('#input-email').bind({
-		focus: function(){
-			$('#error').slideUp();
-			if($(this).val() == 'your@email.com') {
-				$(this).val('');
-			}
-		},
-		blur: function() {
-			if($(this).val() == '') {
-				$(this).val('your@email.com');
-			}
-		}
-	});
+$(document).ready(function() {
 
-	$('#submit-email').click( function(e) {
-		e.preventDefault();
-		$('#error').slideUp();
-		$('#resp').html('');
-		$('#submit-email').attr('disabled', true);
-		if($('#input-email').val().match(/^\S+@\S+\.\S+$/) && launchPage.sent !== true && $('#input-email').val() != 'your@email.com' ) {
-			// Animate background
-			$('.fancy-form').animate({backgroundPositionX: launchPage.formBgWidth}, 2000, function() {
-	    		 $(this).css('background-position-x', '0');
-	  			}
-	  		);
-			// POST email address
-			$.post(launchPage.url, { email: $('#email').val() },
-				function(data) {
-					if(data.match(/class='success'/)){
-						$('#input-email').attr('disabled', true);
-						$('#resp').html(data);
-						$('#resp').animate({ opacity: 'toggle' }, 'slow');
-						$('.hide-me').animate({ opacity: 0 }, 'slow');
-						launchPage.sent = true;
-					} else {
-						$('#submit-email').attr('disabled', false);
-						$('#error').html(data);
-						$('#error').slideDown();
-					}
-			});
-		} else if(launchPage.sent == true) {
+    // Add spin.js to lazy load container
+  // History, and back button
 
-		} else {
-			$('#submit-email').attr('disabled', false);
-			$('#error').html('<p class="error">Please enter a valid email address.</p>');
-			$('#error').slideDown();
-		}
-	});
+  $(function() {
+    jQuery.history.listen()
+    $('.nav .scroll').click(function() {
+      $.history.push('index.html' + $(this).attr('href'));
+    });
+  });
+  
+    $('.lazy-container').spin({
+        color: '#000'
+    });
+
+    // Lazy loading. 
+    $("img.lazy").lazyload({
+        // The image starts loading 200 px before it is in viewport
+        threshold: 200,
+        // Remove the line if you don`t need fade effect. 
+        effect: "fadeIn",
+        // Change this for fade in speed
+        effectspeed: 600,
+        //  Hide spinner when loaded
+        load: function(elements_left, settings) {
+            $(".lazy-container").has(this).addClass('loaded');
+            $(".loaded .spinner").remove();
+            // refresh bootstrap scrollspy, when image is loaded
+            $('[data-spy="scroll"]').each(function() {
+                var $spy = $(this).scrollspy('refresh')
+            });
+        }
+    });
+
+    // Lightbox
+
+    $('.lightbox').magnificPopup({
+        type: 'image',
+        disableOn: function() {
+            // Detect here whether you want to show the popup
+            // return true if you want
+            if ($(window).width() < 500) {
+                return false;
+            }
+            return true;
+        },
+        preloader: true,
+        tLoading: 'Loading',
+
+        // Delay in milliseconds before popup is removed
+        removalDelay: 300,
+        mainClass: 'mfp-fade',
+        callbacks: {
+            open: function() {
+                $('.navbar').fadeOut('slow');
+            },
+            close: function() {
+                $('.navbar').fadeIn('slow');
+            }
+        }
+    });
+
+    // Lightbox video/maps
+
+    $(' .iframe').magnificPopup({
+        type: 'iframe',
+        mainClass: 'mfp-fad',
+        disableOn: function() {
+            if ($(window).width() < 500) {
+                return false;
+            }
+            return true;
+        },
+        preloader: true,
+
+        callbacks: {
+            open: function() {
+                $('.navbar').fadeOut('slow');
+            },
+            close: function() {
+                $('.navbar').fadeIn('slow');
+            }
+        }
+    });
+
+    // .scroll class for link scrolling.
+
+    $('.scroll[href^="#"]').bind('click.smoothscroll', function(e) {
+        e.preventDefault();
+        var target = this.hash;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function() {
+            window.location.hash = target;
+        });
+
+    });
+
+    // Change icons on accardion
+
+    $('.collapse').on('show.bs.collapse', function() {
+        $(this).parent().find(".fa-plus").removeClass("fa-plus").addClass("fa-minus");
+        $(this).parent().find(".panel-heading").addClass("active");
+    }).on('hide.bs.collapse', function() {
+        $(this).parent().find(".fa-minus").removeClass("fa-minus").addClass("fa-plus");
+        $(this).parent().find(".panel-heading").removeClass("active");
+    });
+
+    // Close menu when in mobile view clicked
+    $('.nav .scroll').click(function(e) {
+        if ($('.navbar-toggle').is(":visible"))
+        $("#nav-collapse").removeClass("in").addClass("collapse");
+    });
 });
