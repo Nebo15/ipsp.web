bb654ec2212cab4c04f4100d9a3c44212afeafa4
TonyTekk <thetonytekk@gmail.com>
2014-10-30 22:34:42 +0200

    questionnaire ащкь

---
+++ www/form/js/questionnaire-form.js
@@ -0,0 +1,67 @@
+var form = $("#questionnaire-form"), // form
+    data = [];                       // form data
+
+form.validate({
+    errorPlacement: function errorPlacement(error, element) { 
+        element.before(error);
+    },
+});
+
+form.on('click', '#business-address-check', function(){
+    if (this.checked){
+        adrs = $( "#legal-address :input" ).serializeArray();
+        $.each($( "#business-address :input" ).serializeArray(), function (i, field) { 
+            $( "input[name='" + field.name + "']" ).val(adrs[i].value); 
+            $('.checkbox').css('backgroundImage','url("/img/check.png")');
+        });
+    } else {
+        $( "#business-address :input" ).val('');
+        $('.checkbox').css('backgroundImage','url("/img/uncheck.png")');
+    }     
+});
+
+form.steps({
+    headerTag       : "h3",
+    bodyTag         : "section",
+    transitionEffect: "slideLeft",
+    stepsOrientation: "horizontal",
+    labels: { next: 'next', previous: 'back', finish: 'submit' },
+    
+    onStepChanging: function (event, currentIndex, newIndex) {
+        form.validate().settings.ignore = ":disabled,:hidden";
+        return newIndex < currentIndex ? true : form.valid(); // Step validation
+        //return true; 
+    },
+
+    onStepChanged: function (event, currentIndex, priorIndex){
+        currentIndex++;
+        priorIndex++;
+        $('#questionnaire-form-step-label').html('step ' + currentIndex);
+        $('#step-' + currentIndex).css('display','block');
+        $('#step-' + priorIndex).css('display','none');
+    },
+
+    onFinishing: function (event, currentIndex)
+    {
+        if ( form.valid() ) {
+            $.each(form.serializeArray(), function (i, field) {
+                item = {};
+                item["field"] = field.name;
+                item["value"] = field.value;
+                data.push(item);
+            });
+            console.log(JSON.stringify(data));
+
+            $.ajax({
+                type: "POST",
+                url: 'send.php',
+                data: {'data' : JSON.stringify(data)},
+                success: function (data) {  
+                    alert('Done!');
+                }    
+            });
+        } else {
+            alert('Please, fill all the required fields.');
+        }  
+    }
+}); 
---
+++ www/form/send.php
@@ -0,0 +1,48 @@
+<?php
+require_once(dirname(dirname(dirname(__FILE__))).'/vendor/autoload.php');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $mc    = new Mailchimp('3868f86110f53230458abc41c7bbd0de-us7', ['CURLOPT_FOLLOWLOCATION' => true]);
+    $data  = json_decode($_POST['data']);
+    $email = $data[21];
+    $vars  = [
+        'EMAIL'      => $email->value,
+        //-------------- 1 step -------------    
+        'LEGALADDR1' => $data[0]->value,
+        'LEGALADDR2' => $data[1]->value,
+        'LEGALADDR3' => $data[2]->value,
+        'LEGALADDR4' => $data[3]->value,
+        'COUNTRY'    => $data[4]->value,
+        'BUSINESSA1' => $data[5]->value,
+        'BUSINESSA2' => $data[6]->value,
+        'BUSINESSA3' => $data[7]->value,
+        'BUSINESSA4' => $data[8]->value,
+        'BUSSINESS'  => $data[9]->value,
+        //-------------- 2 step ------------- 
+        'WEBSITES'   => $data[10]->value,
+        'DESCRIBES'  => $data[11]->value,  
+        'DOESBUSIN'  => $data[12]->value,
+        //-------------- 3 step ------------- 
+        'WHATISPA'   => $data[13]->value,  
+        'WHATISTO'   => $data[14]->value, 
+        'WHATISAN'   => $data[15]->value,
+        'WHATISA'    => $data[16]->value,
+        'WHICHGEOG'  => $data[17]->value,
+        //-------------- 4 step -------------   
+        'PRIMARYMA1' => $data[18]->value, 
+        'PRIMARYMA2' => $data[19]->value . '/' . $data[20]->value,
+        'PRIMARYMA4' => $data[21]->value,
+        'PRIMARYMA5' => $data[22]->value,
+        'ITSPECIAL1' => $data[23]->value,
+        'ITSPECIAL2' => $data[24]->value . '/' . $data[25]->value,
+        'ITSPECIAL4' => $data[26]->value,
+        'ITSPECIAL5' => $data[27]->value,
+        'OTHERISSU1' => $data[28]->value,
+        'OTHERISSU2' => $data[29]->value . '/' . $data[30]->value,
+        'OTHERISSU4' => $data[31]->value,
+        'OTHERISSU5' => $data[32]->value
+    ];
+
+    $result = $mc->lists->subscribe('e1fc4230c1', ['email' => $email->value], $vars, 'html', false, true);
+}
+?>
